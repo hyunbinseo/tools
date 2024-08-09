5204be9415c7486ddaabe4edb6bc46dcb95dd446
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-08-09 13:08:03 +0900

    refactor: remove types dir

--- src/modules/date.js
+++ src/modules/date.js
@@ -1,6 +1,14 @@
-import { timezoneToMinutes } from './timezone.js';
+/** @import {Timezone} from "./date.types.js" */
 
-/** @param {import("../types/timezone.js").Timezone} timezone */
+/** @param {Timezone} timezone */
+const timezoneToMinutes = (timezone) => {
+	const multiplier = Number(timezone.substring(0, 1) + '1');
+	const hour = Number(timezone.substring(1, 3));
+	const minute = Number(timezone.substring(4));
+	return multiplier * (60 * hour + minute);
+};
+
+/** @param {Timezone} timezone */
 export const dateToISOStringWithOffset = (date = new Date(), timezone) => {
 	const shifted = new Date(date.valueOf() + timezoneToMinutes(timezone) * 60 * 1000);
 	return shifted.toISOString().substring(0, 19) + timezone;
--- src/modules/timezone.js
+++
@@ -1,7 +0,0 @@
-/** @param {import("../types/timezone.js").Timezone} timezone */
-export const timezoneToMinutes = (timezone) => {
-	const multiplier = Number(timezone.substring(0, 1) + '1');
-	const hour = Number(timezone.substring(1, 3));
-	const minute = Number(timezone.substring(4));
-	return multiplier * (60 * hour + minute);
-};
