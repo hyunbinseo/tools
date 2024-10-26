206bd5d77cadb64246e820ef23b5a3c72ab879fa
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-10-26 21:27:28 +0900

    feat: autocomplete nullable keys only

--- src/record.ts
+++ src/record.ts
@@ -1,11 +1,16 @@
 // Reference https://github.com/microsoft/TypeScript/issues/28374#issuecomment-1907275765
 
-type DeepNonNullable<T> = { [P in keyof T]-?: NonNullable<T[P]> };
+type NullableKeys<T> = {
+	[K in keyof T]: null extends T[K] ? K : undefined extends T[K] ? K : never;
+}[keyof T];
 
-export const hasNonNullableValues = <T extends Record<string, unknown>, K extends Array<keyof T>>(
+export const hasNonNullableValues = <
+	T extends Record<string, unknown>, //
+	K extends NullableKeys<T>,
+>(
 	obj: T,
-	keys: K,
-): obj is Omit<T, K[number]> & Pick<DeepNonNullable<T>, K[number]> => {
+	keys: K[],
+): obj is T & { [P in K]: NonNullable<T[P]> } => {
 	for (const key of keys) {
 		if (obj[key] === null || obj[key] === undefined) return false;
 	}
